61c284ffe70ac5ff1dd30064e5b853c2f892f44c
Charles Samuels <chasms@chas.ms>
2025-06-25 15:52:41 -0400

    Initial transposition of content from old to new homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,126 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { styled } from "../styled-system/jsx";
-
-const StyledListItem = styled.li`
-  font-size: 72px;
-`
+import { HomeWrapper, LogoOverlay, LogoWrapper } from "./page.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+    <HomeWrapper>
+      <LogoOverlay>
+        <LogoWrapper>
+          <img src="img/kundalini-white.png" alt="chasms"></img>
+        </LogoWrapper>
+      </LogoOverlay>
+
+      <nav className="reveal">
+        <ul id="chasms_head">
+          <li id="chasm">
+            <p>
+              CH<strong>ARLES </strong>A<strong>LBERT </strong>S
+              <strong>A</strong>M<strong>UEL</strong>S
+            </p>
           </li>
+        </ul>
 
-          <StyledListItem>Save and see your changes instantly.</StyledListItem>
-        </ol>
+        <ul className="navigation">
+          <li>
+            <a
+              className="nav-link"
+              href="https://www.medium.com/@chasms"
+              target="_blank"
+            >
+              BLOG
+            </a>
+          </li>
+          <li>
+            <a
+              className="nav-link"
+              href="https://www.github.com/chasms"
+              target="_blank"
+            >
+              GITHUB
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="info.html">
+              ABOUT
+            </a>
+          </li>
+        </ul>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <ul className="icons">
+          <li className="icon">
+            <a
+              href="mailto:chasms@chas.ms"
+              className="sb small no-shadow border circle email"
+            >
+              Email
+            </a>
+          </li>
+          <li className="icon">
+            <a
+              href="https://linkedin.com/in/chasms"
+              target="_blank"
+              className="sb small no-shadow border circle linkedin"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="content-area">
+        <div className="background" id="main"></div>
+
+        <div className="assets reveal">
+          <a className="window" href="SP.html">
+            <img
+              alt="Salt & Pepper Shakers"
+              className="work"
+              src="/img/SP0.png"
             />
-            Deploy now
+            <div className="subtitle bk">
+              <p>
+                <strong>semantic</strong> shakers
+              </p>
+            </div>
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
+
+          <a className="window" href="JS.html">
+            <img alt="" className="work" src="/img/JS0.png" />
+            <div className="subtitle bk">
+              <p>Presentation on Javascript Transpilers</p>
+            </div>
+          </a>
+
+          <a className="window" href="SK.html">
+            <img alt="" className="work" src="/img/SK0.png" />
+            <div className="subtitle bk">
+              <img alt="" className="banner" src="/img/SKCsm.png" />
+            </div>
+          </a>
+
+          <a className="window" href="PP.html">
+            <img alt="" className="work" src="/img/PP0.png" />
+            <div className="subtitle bk">
+              <p>
+                <strong>ergonomic</strong> peelers
+              </p>
+            </div>
+          </a>
+
+          <a className="window" href="F.html">
+            <img alt="" className="work" src="/img/F0.png" />
+            <div className="subtitle bk">
+              <p>furniture</p>
+            </div>
+          </a>
+
+          <a className="window" href="MG.html">
+            <img alt="" className="work" src="/img/MG0.png" />
+            <div className="subtitle bk">
+              <p>new orleans</p>
+            </div>
           </a>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </HomeWrapper>
   );
 }
