ccba49abcb21f674ad7951c0a53680a6be0a6201
Charles Samuels <chasms@chas.ms>
2025-07-03 15:15:22 -0400

    Added iframe to enable unmounting of p5 experience

--- app/routes/p5-space.tsx
+++ app/routes/p5-space.tsx
@@ -1,3 +1,4 @@
+import { css } from "../../styled-system/css";
 import Nav from "../components/Nav";
 import type { Route } from "./+types/p5-space";
 
@@ -5,20 +6,17 @@ export function meta({}: Route.MetaArgs) {
   return [{ title: "p5 Space" }, { name: "description", content: "p5 Space" }];
 }
 
+const iframe = css({
+  width: "100%",
+  height: "100%",
+});
+
 export default function p5Space() {
   return (
     <>
       <Nav />
 
-      <script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js"></script>
-      <script src="https://cdn.jsdelivr.net/gh/freshfork/p5.EasyCam@1.2.1/p5.easycam.min.js"></script>
-
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.sound.min.js"></script>
-
-      <script src="https://unpkg.com/p5.touchgui@0.5.2/lib/p5.touchgui.js"></script>
-
-      <script src="p5-space.js"></script>
+      <iframe className={iframe} src="./p5-space/p5-space.html" />
     </>
   );
 }
