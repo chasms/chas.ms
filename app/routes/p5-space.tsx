c7450d921ea97d381a5e33ae08c4967ab7a7ef00
Charles Samuels <chasms@chas.ms>
2025-07-07 02:58:48 -0400

    Added github logo link to p5-space page

--- app/routes/p5-space.tsx
+++ app/routes/p5-space.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router";
 import { css } from "../../styled-system/css";
+import githubIcon from "../assets/icons/github.png";
 import Nav from "../components/Nav";
 import { toHomeWithOpeningSequenceComplete } from "../routes";
 
@@ -11,12 +13,45 @@ const iframe = css({
   height: "100%",
 });
 
+const githubTab = css({
+  position: "absolute",
+  bottom: "25px",
+  right: "25px",
+  height: "60px",
+  width: "60px",
+  display: "flex",
+  borderRadius: "50%",
+  backdropFilter: "invert(20%)",
+  transition: "all 0.5s ease-in-out",
+
+  _hover: {
+    backdropFilter: "invert(80%)",
+    height: "80px",
+    width: "80px",
+  },
+});
+
+const githubImg = css({
+  height: "40px",
+  width: "40px",
+  margin: "auto",
+  opacity: "0.7",
+});
+
 export default function p5Space() {
   return (
     <>
       <Nav backButtonTo={toHomeWithOpeningSequenceComplete} />
 
       <iframe className={iframe} src="https://p5-space.chas.ms" />
+
+      <Link
+        className={githubTab}
+        to="https://github.com/chasms/p5-space"
+        target="_blank"
+      >
+        <img className={githubImg} src={githubIcon} />
+      </Link>
     </>
   );
 }
