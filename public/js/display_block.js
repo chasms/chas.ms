8cd74a05dc8a0ff2cd85d522bb34dbac4b882b3c
Charles Samuels <chasms@chas.ms>
2025-08-04 16:08:14 -0400

    Reorganized and pruned old files

--- public/js/display_block.js
+++
@@ -1,7 +0,0 @@
-function display_block(className) {
-  var e = getElementsByClassName(className);
-  for (var i = 0, len = e.length; i < len; ++i) {
-    if (e[i].style.display == "block") e[i].style.display = "block";
-    else e[i].style.display = "block";
-  }
-}
