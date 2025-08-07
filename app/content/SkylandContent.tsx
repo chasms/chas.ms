dfcbdeb6167b8ef7fb3624a82adc725a1fb61730
Charles Samuels <chasms@chas.ms>
2025-08-07 19:21:18 -0400

    Skyland content changes

--- app/content/SkylandContent.tsx
+++ app/content/SkylandContent.tsx
@@ -5,6 +5,7 @@ export default function SkylandContent() {
     <div className={contentWrapper}>
       <h1 className={contentHeading}>Skyland</h1>
       <p className={contentBody}>A collection of photos from Skyland.</p>
+      <p className={contentBody}>Zine created and published in 2015.</p>
     </div>
   );
 }
