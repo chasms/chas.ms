d44822d87da46f2f28c9a9531030f07381f761de
Charles Samuels <chasms@chas.ms>
2025-09-17 16:43:38 -0400

    Updated HomeProjectsGallery to use object and map through projects

--- app/components/HomeProjectsGallery.tsx
+++ app/components/HomeProjectsGallery.tsx
@@ -51,188 +51,201 @@ const galleryBannerText = css({
   },
 });
 
+const HomeGalleryItems = [
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.giantmachines}`,
+    image: GMGif,
+    imageAltText: "Giant Machines",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Giant Machines</strong> Acquired by Deloitte
+      </p>
+    ),
+  },
+  {
+    href: "https://roll2write.chas.ms",
+    newTab: true,
+    image: Roll2WriteThumbnail,
+    imageAltText: "Roll2Write",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Roll2Write</strong>, a songwriting oracle
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.onmyway}`,
+    image: OMWThumbnail,
+    imageAltText: "On My Way",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>On My Way</strong> National Grid
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs["p5-space"]}`,
+    image: P5SpaceGif,
+    imageAltText: "P5 Space Project",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        p5 <strong>Space</strong>
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.convene}`,
+    image: ConveneThumbnail,
+    imageAltText: "Convene",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Convene</strong> Bookings and Payments
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys["fin-js"]}`,
+    image: FinJSGif,
+    imageAltText: "FinJS",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Desktop App Interoperability,</strong> FinJS 2019
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.nota}`,
+    image: NotaThumbnail,
+    imageAltText: "Nota",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Nota</strong>, An M&T Bank Product
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.indonesia}`,
+    image: IndonesiaThumbnail,
+    imageAltText: "Yogyakarta Indonesia Photographs",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Yogyakarta</strong>, Indonesia, 2014
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.printmaking}`,
+    image: PrintmakingThumbnail,
+    imageAltText: "Printmaking",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Printmaking</strong>
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.reflections}`,
+    image: ReflectionsThumbnail,
+    imageAltText: "Extended Reflections Photographs",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        Extended <strong>Reflections</strong>, 2011
+      </p>
+    ),
+  },
+  {
+    href: RouteSlugs["origin-theory"],
+    image: OriginTheoryThumbnail,
+    imageAltText: "Origin Theory",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Origin</strong> Theory, 2014
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys["salt-and-pepper"]}`,
+    image: ShakersThumbnail,
+    imageAltText: "Salt & Pepper Shakers",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>semantic</strong> shakers
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.skyland}`,
+    image: SkylandThumbnail,
+    imageAltText: "",
+    bannerElement: <img alt="s" className="banner" src={SkylandBanner} />,
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.peelers}`,
+    imageAltText: "Potato Peelers",
+    image: PeelerThumbnail,
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>ergonomic</strong> peelers
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys.furniture}`,
+    image: FurnitureGalleryImage,
+    imageAltText: "Furniture",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>furniture</strong>
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys["mardi-gras"]}`,
+    image: MardiGrasThumbnail,
+    imageAltText: "Mardi Gras",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>mardi gras</strong>, new orleans, 2014
+      </p>
+    ),
+  },
+  {
+    href: "https://olio.chas.ms",
+    newTab: true,
+    image: OlioThumbnail,
+    imageAltText: "Olio",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>olio app</strong>, a collection of things
+      </p>
+    ),
+  },
+  {
+    href: `/${RouteSlugs.project}/${GalleryKeys["js-transpilers"]}`,
+    image: JavascriptTranspilersThumbnail,
+    imageAltText: "JavaScript Transpilers Presentation",
+    bannerElement: (
+      <p className={galleryBannerText}>
+        <strong>Presentation on Transpilers</strong>, 2017
+      </p>
+    ),
+  },
+];
+
 export default function HomeProjectsGallery() {
   return (
     <div className={gallery}>
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.giantmachines}`}
-        image={GMGif}
-        imageAltText="Giant Machines"
-      >
-        <p className={galleryBannerText}>
-          <strong>Giant Machines</strong> Acquired by Deloitte
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href="https://roll2write.chas.ms"
-        newTab
-        image={Roll2WriteThumbnail}
-        imageAltText="Roll2Write"
-      >
-        <p className={galleryBannerText}>
-          <strong>Roll2Write</strong>, a songwriting oracle
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.onmyway}`}
-        image={OMWThumbnail}
-        imageAltText="On My Way"
-      >
-        <p className={galleryBannerText}>
-          <strong>On My Way</strong> National Grid
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs["p5-space"]}`}
-        image={P5SpaceGif}
-        imageAltText="P5 Space Project"
-      >
-        <p className={galleryBannerText}>
-          p5 <strong>Space</strong>
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.convene}`}
-        image={ConveneThumbnail}
-        imageAltText="Convene"
-      >
-        <p className={galleryBannerText}>
-          <strong>Convene</strong> Bookings and Payments
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys["fin-js"]}`}
-        image={FinJSGif}
-        imageAltText="FinJS"
-      >
-        <p className={galleryBannerText}>
-          <strong>Desktop App Interoperability,</strong> FinJS 2019
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.nota}`}
-        image={NotaThumbnail}
-        imageAltText="Nota"
-      >
-        <p className={galleryBannerText}>
-          <strong>Nota</strong>, An M&T Bank Product
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.indonesia}`}
-        image={IndonesiaThumbnail}
-        imageAltText="Yogyakarta Indonesia Photographs"
-      >
-        <p className={galleryBannerText}>
-          <strong>Yogyakarta</strong>, Indonesia, 2014
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.printmaking}`}
-        image={PrintmakingThumbnail}
-        imageAltText="Printmaking"
-      >
-        <p className={galleryBannerText}>
-          <strong>Printmaking</strong>
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.reflections}`}
-        image={ReflectionsThumbnail}
-        imageAltText="Extended Reflections Photographs"
-      >
-        <p className={galleryBannerText}>
-          Extended <strong>Reflections</strong>, 2011
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={RouteSlugs["origin-theory"]}
-        image={OriginTheoryThumbnail}
-        imageAltText="Origin Theory"
-      >
-        <p className={galleryBannerText}>
-          <strong>Origin</strong> Theory, 2014
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys["salt-and-pepper"]}`}
-        image={ShakersThumbnail}
-        imageAltText="Salt & Pepper Shakers"
-      >
-        <p className={galleryBannerText}>
-          <strong>semantic</strong> shakers
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.skyland}`}
-        image={SkylandThumbnail}
-        imageAltText=""
-      >
-        <img alt="s" className="banner" src={SkylandBanner} />
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.peelers}`}
-        imageAltText="Potato Peelers"
-        image={PeelerThumbnail}
-      >
-        <p className={galleryBannerText}>
-          <strong>ergonomic</strong> peelers
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys.furniture}`}
-        image={FurnitureGalleryImage}
-        imageAltText="Furniture"
-      >
-        <p className={galleryBannerText}>
-          <strong>furniture</strong>
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys["mardi-gras"]}`}
-        image={MardiGrasThumbnail}
-        imageAltText="Mardi Gras"
-      >
-        <p className={galleryBannerText}>
-          <strong>mardi gras</strong>, new orleans, 2014
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href="https://olio.chas.ms"
-        newTab
-        image={OlioThumbnail}
-        imageAltText="Olio"
-      >
-        <p className={galleryBannerText}>
-          <strong>olio app</strong>, a collection of things
-        </p>
-      </HomeProjectGalleryItem>
-
-      <HomeProjectGalleryItem
-        href={`/${RouteSlugs.project}/${GalleryKeys["js-transpilers"]}`}
-        image={JavascriptTranspilersThumbnail}
-        imageAltText="JavaScript Transpilers Presentation"
-      >
-        <p className={galleryBannerText}>
-          <strong>Presentation on Transpilers</strong>, 2017
-        </p>
-      </HomeProjectGalleryItem>
+      {HomeGalleryItems.map((project) => (
+        <HomeProjectGalleryItem
+          key={project.href}
+          href={project.href}
+          newTab={project.newTab}
+          image={project.image}
+          imageAltText={project.imageAltText}
+        >
+          {project.bannerElement}
+        </HomeProjectGalleryItem>
+      ))}
     </div>
   );
 }
